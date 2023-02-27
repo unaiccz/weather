85c9f41198b9a0ed2818593790ff4e016e08d095
unaiccz <unaicc2003@gmail.com>
2023-02-27 09:13:08 +0100

    handle

--- src/components/Get.js
+++ src/components/Get.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 function Get() {
+const [inp,setinp] =useState('');
+const handle = ({target}) => {
+setinp(target.value)
+
+
+}
+const submit =(e)=>{
+e.preventDefault()
+    console.log(inp)
+}
     return ( <>
     <div className='card'>
 
 <h3>Weather</h3>
 <div className='card-body'>
-<input type='text' placeholder='Ubicacion' id='buscador'/>
+<form onSubmit={submit}>
+<input type='text' placeholder='Ubicacion' id='buscador' onChange={handle}
+value={inp}
+/>
+<input type= 'submit' value='send'/>
+<img src='img/nb.png'/>
+</form>
 
 </div>
     </div>
