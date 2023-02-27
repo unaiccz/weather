5e90cdf454f67af7c39b10cf92f2840acdb26192
unaiccz <unaicc2003@gmail.com>
2023-02-27 08:46:02 +0100

    initial

---
+++ src/components/Get.js
@@ -0,0 +1,26 @@
+import React from 'react';
+function Get() {
+    return ( <>
+    <div className='card'>
+
+<h3>Weather</h3>
+<div className='card-body'>
+<input type='text' placeholder='Ubicacion' id='buscador'/>
+
+</div>
+    </div>
+    </>
+    
+    
+    );
+}
+
+export default Get;<>
+<div className='card'>
+
+<h3>Weather</h3>
+
+</div>
+</>
+
+
